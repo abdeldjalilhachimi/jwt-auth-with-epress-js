6cea0ecb61c4699f074a98d0b213a9ec32b4ec82
Abdeldjalil Hachimi <abdeldjalilhachimi12@gmail.com>
2021-09-09 23:17:03 +0100

    auth using jwt and express js

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+const posts = [
+  {
+    username: "Abdeldjalil",
+    title: "How to learn JS",
+  },
+  {
+    username: "Aissa",
+    title: "How to build Design System",
+  },
+  {
+    username: "Nasser",
+    title: "How to creat React js Components",
+  },
+];
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(3000);
